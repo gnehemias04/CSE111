0b89ba596a0844e2d7ccc86317d28bf5c634bd66
gnehemias04 <gnehemias04@gmail.com>
2026-09-01 22:13:31 -0300

    first assignment ready

--- pyton-review.py
+++ pyton-review.py
@@ -1,1 +1,11 @@
-print("hello world")
+# W01 Learning Activities: Python Review
+
+print("When you physically exercise to strengthen your heart, you should maintain your heart rate within a range for at least 20 minutes. To find that range, subtract your age from 220. This difference is your maximum heart rate per minute. Your heart simply will not beat faster than this maximum (220 - age). When exercising to strengthen your heart, you should keep your heart rate between 65% and 85% of your heart’s maximum rate.")
+
+
+age = int(input("please enter you age: "))
+rate = 220 - age
+low_percentage = int(rate * 0.65)
+high_percentage = int(rate * 0.850)
+
+print (f"When you exercise to strengthen your heart, you should keep your heart rate between {low_percentage} and {high_percentage} beats per minute. ")
